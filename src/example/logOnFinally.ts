d11d651ae2873ca4173afb7bdf12dcffaf8ff7d0
Philipp Munin <pmunin@twilio.com>
2024-04-18 01:11:21 -0400

    refactoring types

--- src/example/logOnFinally.ts
+++ src/example/logOnFinally.ts
@@ -1,9 +1,10 @@
 import { TryCatchFinallyHooksBuilder } from "../TryCatchFinallyHooks";
 
-export const logOnFinally = TryCatchFinallyHooksBuilder.createHook<{ args: { name: string; }; }>(ctx => {
+export const logOnFinally = TryCatchFinallyHooksBuilder.createHook<{ args: { name?: string; }; name:string }>(ctx => {
+  if(!ctx.name) ctx.name = ctx.args.name || ctx.func.name || '<anonymous>'
   return {
     onFinally() {
       console.log(`Function ${ctx.args.name} finished!`);
-    }
+    },
   };
 });
--- src/example/tracker.ts
+++ src/example/tracker.ts
@@ -3,32 +3,35 @@ import { logOnFinally } from "./logOnFinally";
 import { measureDuration } from "../measureDuration";
 import { TryCatchFinallyHooksBuilder, ContextOf } from "../TryCatchFinallyHooks";
 
-const track = (() => {
-  const t1 = new TryCatchFinallyHooksBuilder()
-    .add(callStack)
-    .add(measureDuration)
-    .add(logOnFinally)
-    .createAndAdd(ctx => {
-      return {
-        onFinally() {
-          const datadog: any = {};
-          datadog.histogram(`action-${ctx.args.name}-duration`, ctx.duration, { tags: ['action:' + ctx.args.name, 'callstack:' + ctx.callstack.map(c => c.args.name).join('/')] });
-        },
-      };
-    });
-
-  return t1.createAndAdd<{ defer(fnToDefer: (ctx: ContextOf<typeof t1>) => void): void; }>(ctx => {
-    const funcsToDefer: (() => void)[] = [];
-    ctx.defer = funcsToDefer.push.bind(funcsToDefer);
+const track = new TryCatchFinallyHooksBuilder()
+  .add(callStack)
+  .add(measureDuration)
+  .add(logOnFinally)
+  .createAndAdd(ctx => {
     return {
       onFinally() {
+        const datadog: any = {};
+        datadog.histogram(`action-${ctx.args.name}-duration`, ctx.duration, { tags: ['action:' + ctx.name, 'callstack:' + ctx.callstack.map(c => c.name).join('/')] });
+      },
+    };
+  })
+  .createAndAdd((ctx) => {
+    type DeferContext = typeof ctx
+    const funcsToDefer = [] as ((_ctx:DeferContext) => void)[]
+    return {
+      context: {
+        defer(fn:(_ctx:DeferContext)=>void) {
+          funcsToDefer.push(fn)
+        }
+      },
+      onFinally(_ctx) {
         for (const defer of funcsToDefer) {
-          defer();
+          defer(_ctx);
         }
       }
-    };
-  });
-})();
+    }
+  })
+;
 const myTrackedFunction = track.asFunctionWrapper({ name: 'MyAction' })(
   function myTrackedFunction(a: number, b: number) {
     track.current!.defer((op) => {
